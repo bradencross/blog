2006e050f705cbf098fae13a7a003646795594a6
bradencross <bradendcross@gmail.com>
2025-06-13 21:30:25 -0700

    Cyber punk theme

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,5 @@
+
+import __ASTRO_IMAGE_IMPORT_MGbyL from "../../assets/blog-placeholder-5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx";
+import __ASTRO_IMAGE_IMPORT_ZulDpU from "../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md";
+export default new Map([["../../assets/blog-placeholder-5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx", __ASTRO_IMAGE_IMPORT_MGbyL], ["../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md", __ASTRO_IMAGE_IMPORT_ZulDpU]]);
+		
