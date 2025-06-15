71327bd6e440affdb889066305c3957753b381b6
Braden <bradendcross@gmail.com>
2025-06-15 13:52:06 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://bradencross.com',
+	site: 'https://www.bradencross.com',
 	base: '/blog',
 	output: 'static',
 	integrations: [mdx(), sitemap()],
