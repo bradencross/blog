eba4f10ece2fb85929bceb9f3cd499d42ca0a1c2
bradencross <bradendcross@gmail.com>
2025-06-15 14:02:33 -0700

    Modify the config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.bradencross.com',
-	base: '/blog',
+	base: '/',
 	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
