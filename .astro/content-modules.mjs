74a682734157e22f6034abbb3647588920944aa2
Braden Cross <bradendcross@gmail.com>
2025-08-03 09:01:57 -0700

    Create drafts folder

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,7 @@
 
 export default new Map([
 ["src/content/blog/creating-blog-with-astro-cursor.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcreating-blog-with-astro-cursor.mdx&astroContentModuleFlag=true")],
-["src/content/blog/idempotency-4-ways.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fidempotency-4-ways.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/idempotency-4-ways.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fidempotency-4-ways.mdx&astroContentModuleFlag=true")],
+["src/content/blog/creating-blog-with-astro-cursor copy.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcreating-blog-with-astro-cursor+copy.mdx&astroContentModuleFlag=true")],
+["src/content/blog/initial-commit.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Finitial-commit.mdx&astroContentModuleFlag=true")]]);
 		
